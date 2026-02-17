e8bd8e6f6ac2c73fa6ad93d10a60ceda3d7fcd6e
lekhang0610 <khangdeptrai0610@gmail.com>
2026-02-17 10:03:47 +0700

    U

--- sw.js
+++ sw.js
@@ -1,38 +1,46 @@
-const CACHE_NAME = 'photobooth-v1';
+const CACHE_NAME = 'photobooth-v2'; // Thay đổi v này khi cập nhật code
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/menu.html',
-  '/camera.html',
-  '/upload.html',
-  '/final.html',
-  '/StyleSheets/home.css',
-  '/StyleSheets/camera.css',
-  '/StyleSheets/final.css',
-  '/Javascripts/utils.js',
-  '/Javascripts/home.js',
-  '/Javascripts/camera.js',
-  '/Javascripts/upload.js',
-  '/Javascripts/final.js',
-  '/Assets/photobooth/logo-new.png',
-  '/Assets/photobooth/shutter.mp3',
-  '/Assets/photobooth/count.mp3'
+  './',
+  './index.html',
+  './menu.html',
+  './camera.html',
+  './upload.html',
+  './final.html',
+  './StyleSheets/home.css',
+  './StyleSheets/camera.css',
+  './StyleSheets/final.css',
+  './Javascripts/utils.js',
+  './Javascripts/home.js',
+  './Javascripts/camera.js',
+  './Javascripts/upload.js',
+  './Javascripts/final.js',
+  './manifest.json',
+  './Assets/photobooth/logo-new.png',
+  './Assets/photobooth/favicon.png',
+  './Assets/photobooth/icon-192.png',
+  './Assets/photobooth/icon-512.png',
+  './Assets/photobooth/shutter.mp3',
+  './Assets/photobooth/count.mp3'
 ];
 
-// Cài đặt Service Worker và lưu trữ tài nguyên
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-// Phản hồi yêu cầu từ bộ nhớ đệm
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
